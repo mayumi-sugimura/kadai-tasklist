d47a4870bb105f71869eb51254dbae7edea2b507
mayumi.sugimura <ma-s@kk-isshinsha.co.jp>
2019-10-12 20:18:20 +0000

    CRUD tasks

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} の詳細ページ</h1>
+    
+    <table class="table tabel-bordered">
+        <tr>
+            <th>{{ $task->id }}</th>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <th>{{ $task->content }}</th>
+        </tr>
+    </table>
+    
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
+    
+    {!!Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+    
+@endsection
